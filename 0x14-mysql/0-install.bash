3a79ed2bfd87c9ce5981cb1939a144ac5ae06dc6
12eazy <oyetolaisreal01@gmail.com>
2024-05-15 09:50:21 -0700

    editing

--- 0x14-mysql/0-install.bash
+++ 0x14-mysql/0-install.bash
@@ -1,7 +1,26 @@
-#!/usr/bin/env bash
-# Installs MySQL server version 5.7.x
-sudo apt-key add './mysql-5.7_signature.key'
-sudo /bin/bash -c 'echo "deb http://repo.mysql.com/apt/ubuntu bionic mysql-5.7" >> /etc/apt/sources.list.d/mysql.list'
-sudo apt-get update
-sudo apt install -f mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7*
-# password: ubuntu_user_pwd
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License, version 2.0, for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+#
+# The MySQL  Server configuration file.
+#
+# For explanations see
+# http://dev.mysql.com/doc/mysql/en/server-system-variables.html
+
+[mysqld]
+pid-file        = /var/run/mysqld/mysqld.pid
+socket          = /var/run/mysqld/mysqld.sock
+datadir         = /var/lib/mysql
+log-error       = /var/log/mysql/error.log
+# By default we only accept connections from localhost
+#bind-address   = 127.0.0.1
+server-id       = 1
+log_bin         = /var/log/mysql/mysql-bin.log
+# Disabling symbolic-links is recommended to prevent assorted security risks
+symbolic-links=0
+
+binlog_do_db    = tyrell_corp
